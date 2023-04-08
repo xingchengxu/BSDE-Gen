81bb8b765c0e0bafed71c6be30ebeed878b33d19
Windstars <xingcheng_cn@163.com>
2023-04-08 21:41:23 +0800

    Update

--- data_loader_x.py
+++ data_loader_x.py
@@ -8,5 +8,5 @@ root = "./data"
 trans = transforms.Compose([transforms.ToTensor(),
                transforms.Lambda(lambda x: (x-0.5)*2) # Scale between [-1, 1]
                ])
-train_set = datasets.FashionMNIST(root=root, train=True, transform=trans, download=True)
-# train_set = datasets.MNIST(root=root, train=True, transform=trans, download=True)
+train_set_fashion = datasets.FashionMNIST(root=root, train=True, transform=trans, download=True)
+train_set_mnist = datasets.MNIST(root=root, train=True, transform=trans, download=True)
